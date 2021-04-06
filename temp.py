c3262e9e10e6903de30a8d0aa724131d7d1e0bb8
Tevfik SASTIM <70819588+talfik2@users.noreply.github.com>
2021-04-06 21:08:31 +0300

    Add files via upload

---
+++ temp.py
@@ -0,0 +1,27 @@
+import numpy as np
+from flask import Flask, request, render_template
+import joblib
+
+app = Flask(__name__, template_folder = 'template')#main klasörüm bu
+model = joblib.load('model.pkl')
+#model = pickle.load(open('model.pkl', 'rb'))
+
+@app.route('/')
+def home():
+    return render_template('index.html')#İlk göreceğimiz şey index.html olsun
+
+@app.route('/predict',methods=['POST'])
+def predict():
+    '''
+    For rendering results on HTML
+    '''
+    int_features = [int(x) for x in request.form.values()]
+    final_features = [np.array(int_features)]
+    prediction = model.predict(final_features)
+    
+    output = round(prediction[0], 2)
+    
+    return render_template('index.html', prediction_text = 'Correct cab company for these given values is  {}'.format(output))
+
+if __name__ == "__main__":
+    app.run(port = 12345, debug = True)
